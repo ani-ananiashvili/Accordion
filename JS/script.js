30adbdd3cb2982963156ba0e4e4fc473dd253034
Ani Ananiashvili <ananiashvili10@gmail.com>
2024-08-03 16:52:12 +0400

    add styles + JS

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+// accordion section
+
+document.querySelectorAll(".accordion").forEach((accordion) => {
+  accordion.addEventListener("click", () => {
+    const answer = accordion.querySelector(".answer");
+    const arrow = accordion.querySelector(".arrow");
+
+    accordion.classList.toggle("active");
+    arrow.classList.toggle("active");
+  });
+});
